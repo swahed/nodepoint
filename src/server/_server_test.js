2062184ef3f42124c95b2558ad7a254c2a981e04
swahed <serjoscha.wahed@gmail.com>
2014-10-12 19:31:03 +0200

    Introduced teardown to server tests

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,9 +1,49 @@
 "use strict";
 
 var server = require("./server.js");
+var http = require("http");
 
-/*global test*/
-exports.testNothing = function(test){
-	test.equals(3, server.number(), "number function");
+exports.setUp = function(done) {
+    /*server.start(8080, function() {
+        callback();
+    });*/
+	done();
+};
+
+exports.tearDown = function(done)
+{
+	//TODO: Check if started
+	if(server.isRunning()) {
+		server.stop(function(){
+			done();
+		});
+	} else {
+		done();
+	}
+};
+
+exports.isCorrectPortNumber = function(test){
+	test.equal(server.getPortNumber(), 8080);
 	test.done();
+};
+
+exports.isServerStartedStatusCorrect = function(test){
+	server.start();
+	test.ok(server.isRunning());
+	test.done();
+};
+
+exports.testServerRespondsToRequests = function(test) {
+	server.start();
+
+	console.log("respond test started");
+	var portNumber = server.getPortNumber();
+	http.get("http://localhost:" + portNumber, function(res){
+		res.on("data", function(){});
+		test.done();
+	});
+	//.on('error', function(e) { // <- TODO
+	//	test.fail();
+	//	test.done();
+	//});
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,30 @@
 "use strict";
 
-console.log("Server started!");
+var http = require("http");
+var server;
+var portNumber = 8080;
+var isRunning = false;
 
-exports.number = function(){
-	return 3;
+exports.getPortNumber = function() {
+	return portNumber;
+};
+
+exports.isRunning = function(){ 
+	return isRunning; // TODO: Check if server object has corresponding functionlity
+};
+
+exports.start = function(){
+	server = http.createServer();
+	server.on("request", function(req, res){
+		res.end('Your node.js server is running on localhost:8080');
+	});
+	server.listen(portNumber);
+	isRunning = true;
+	console.log("Server started");
+};
+
+exports.stop = function(done){
+	server.close(function(){
+		done();
+	});
 };
